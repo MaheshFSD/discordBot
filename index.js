ab0bdc434ebb9ed34daa344e7a89b0b1f5d0aa8f
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-05-28 20:13:54 +0530

    replying to a message with bot done

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ dotenv.config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 console.log(process.env.secret, ' ----------- secete ------- ');
 client.on('messageCreate', (message) => {
+    if(message.author.bot) return;
     console.log(message.content, ' ----------- content created ---------- ');
     console.log(message.author.username, ' -------- username ------- ');
-    message.delete().then((data)=> { console.log(data, 'delete------')}); // trying some ops on messages ... 
+    message.reply("I am the great bot...");
+    // message.delete().then((data)=> console.log(data, 'delete------')); // trying some ops on messages ... 
+    // trying to reply with bot...
 })
 
 client.login(process.env.secret);
